5bef2fdb20c55437eb534582d6a0baf9ac432b3e
Andy B <andybarnov@gmail.com>
2016-11-26 14:57:30 +0100

    refactor comparison logic into separate class

---
+++ comparator.rb
@@ -0,0 +1,45 @@
+require_relative 'qpx_result'
+
+class Comparator
+  def initialize(jsons_one, jsons_two)
+    @jsons_one = jsons_one
+    @jsons_two = jsons_two
+  end
+
+  def render_readable
+    output = prepare_readable(@jsons_one, @jsons_two)
+    p output[0]
+    puts "========="
+    p output[1]
+  end
+
+  private
+
+  def prepare_readable(jsons_one, jsons_two)
+
+    results_one = jsons_one.map do |json|
+      QPXResult.new(json)
+    end
+
+    results_two = jsons_two.map do |json|
+      QPXResult.new(json)
+    end
+
+    readable_one = results_one.map do |result|
+      result.trips.map do |trip|
+        [trip.price, trip.destination_city, trip.destination_airport]
+      end
+    end
+
+    readable_two = results_two.map do |result|
+      result.trips.map do |trip|
+        [trip.price, trip.destination_city, trip.destination_airport]
+      end
+    end
+
+    # We need to flatten mapped array before soring
+    readable_one = readable_one.flatten(1).sort
+    readable_two = readable_two.flatten(1).sort
+    [readable_one, readable_two]
+  end
+end
--- obj_sandbox.rb
+++ obj_sandbox.rb
@@ -1,32 +1,36 @@
 require 'json'
 require_relative 'json_results'
 require_relative 'qpx_result'
+require_relative 'comparator'
 
 jsons_one = JSONResults::FROM_ORIGIN1
 jsons_two = JSONResults::FROM_ORIGIN2
 
-results_one = jsons_one.map do |json|
-  QPXResult.new(json)
-end
+comparator = Comparator.new(jsons_one, jsons_two)
+comparator.render_readable
 
-results_two = jsons_two.map do |json|
-  QPXResult.new(json)
-end
+# results_one = jsons_one.map do |json|
+#   QPXResult.new(json)
+# end
+#
+# results_two = jsons_two.map do |json|
+#   QPXResult.new(json)
+# end
 
-readable_one = results_one.map do |result|
-  result.trips.map do |trip|
-    [trip.price, trip.destination_city, trip.destination_airport]
-  end
-end
-readable_one = readable_one.flatten(1).sort
-
-readable_two = results_two.map do |result|
-  result.trips.map do |trip|
-    [trip.price, trip.destination_city, trip.destination_airport]
-  end
-end
-readable_two = readable_two.flatten(1).sort
-
-p readable_one
-puts "==========="
-p readable_two
+# readable_one = results_one.map do |result|
+#   result.trips.map do |trip|
+#     [trip.price, trip.destination_city, trip.destination_airport]
+#   end
+# end
+# readable_one = readable_one.flatten(1).sort
+#
+# readable_two = results_two.map do |result|
+#   result.trips.map do |trip|
+#     [trip.price, trip.destination_city, trip.destination_airport]
+#   end
+# end
+# readable_two = readable_two.flatten(1).sort
+#
+# p readable_one
+# puts "==========="
+# p readable_two
